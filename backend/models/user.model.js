210a18c914ad4d9d6e0b5bd480662a1fc3552782
Mohammad <mohammadkh12221@gmail.com>
2024-10-10 18:23:42 +0100

    created User model and schema

---
+++ backend/models/user.model.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    fullName:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    username:{
+        type:String,
+        required:true,
+        unique:true,
+        trim:true
+    },
+    password:{
+        type:String,
+        required:true,
+        minlength:6
+    },
+    gender:{
+        type:String,
+        enum: ["Male", "Female"],
+        required:true
+    },
+    profilePic:{
+        type:String,
+        default: ""
+    }
+});
+
+const User = mongoose.model("User" , userSchema);
+
+export default User;
