965b9e1588e4457faa5c7c6c4a0bb82143037de8
Mohammad <mohammadkh12221@gmail.com>
2024-10-14 16:17:14 +0100

    implement sidebar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 import Login from './pages/login/Login'
 import Signup from './pages/signup/Signup'
+import Home from './pages/home/Home'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className='p-4 h-screen flex items-center justify-center'>
-      <Signup />
+      <Home />
     </div>
   )
 }
