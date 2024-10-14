5ddc7849cc464df9c4e1b7e3ff9930a76130d8cb
Mohammad <mohammadkh12221@gmail.com>
2024-10-14 16:37:56 +0100

    message and messageContainer

---
+++ frontend/src/components/MessageContainer.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Messages from './MessageContainer/Messages'
+import MessageInput from './MessageContainer/MessageInput'
+
+function MessageContainer() {
+  return (
+    <div className='md:min-w-[450px] flex flex-col '>
+      <>
+      {/* HEADER */}
+      <div className='bg-slate-500 px-4 py-2 mb-2 '>
+        <span className='label-text'>To : </span><span className='text-gray-900 font-bold'>Jhon</span>
+      </div>
+      <Messages />
+      <MessageInput />
+      </>
+    </div>
+  )
+}
+
+export default MessageContainer
